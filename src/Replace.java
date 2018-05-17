93da4396433903b90bc8a89508564dfb517f2984
kubajj <vilouskubajj@gmail.com>
2018-05-18 00:15:17 +0200

    Add Replace.java

---
+++ src/Replace.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+
+public class Replace {
+	public static String replace(String text, Map<String,String> replacement) {
+		for (String k : replacement.keySet()) {				
+			text = text.replaceAll("\\{\\{[ ]* " + k + "[ ]*\\}\\}", replacement.get(k));				
+		}	
+		return text;
+	}
+}
