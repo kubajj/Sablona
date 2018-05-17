5f6c65ef39f1460d9cafe8028a1969742ea37102
kubajj <vilouskubajj@gmail.com>
2018-05-18 00:14:07 +0200

    Add Templater.java

--- src/Templater.java
+++ src/Templater.java
@@ -1,35 +1,24 @@
-
-import java.util.List;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Templater {
 	public static void main(String[] args) {
-		Map<String, String> replacement= new HashMap<>();
-		List<String> keys = new LinkedList<>();
+		Map<String, String> replacement= new HashMap<>();		
 		for (String a : args) {
 			String[] spliteda = a.split("=");
 			if (!spliteda[0].equals("--var")) {
 				continue;
 			} else {	
-				spliteda[1] = "__ " + spliteda[1] + " __";
-				replacement.put(spliteda[1], spliteda[2]);
-				keys.add(spliteda[1]);
+				replacement.put(spliteda[1], spliteda[2]);				
 			}
 		}
-		Scanner template = new Scanner(System.in);
-		while (template.hasNextLine()) {			
-			String line = template.nextLine();
-			for (String k : keys) {				
-				//line.replaceAll("\\{\\{ " + k + " \\}\\}", replacement.get(k));
-				line.replaceAll(k, replacement.get(k));
-			}	
-			System.out.println(line);
+		Scanner sc = new Scanner(System.in);
+		String text = "";
+		while (sc.hasNextLine()) {
+			text += sc.nextLine() + "\n";
 		}
-		for (String h : keys) {	
-			System.out.println(h + "--->" + replacement.get(h));
-		}
+		String print = Replace.replace(text,replacement);
+		System.out.println(print);
 	}
 }
